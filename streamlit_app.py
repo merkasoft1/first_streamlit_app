ceabab29ae6bb979b8212aad24941c004c40b4ee
merkasoft1 <144548574+merkasoft1@users.noreply.github.com>
2023-09-09 14:41:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pandas
 
 
 streamlit.title('Merk\'s House of Horrors')
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Build Your Own Fruit Smoothie')
