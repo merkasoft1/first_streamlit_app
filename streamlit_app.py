8cf33a6eaad692aa942844333d3ed2a18ba3f2c4
merkasoft1 <144548574+merkasoft1@users.noreply.github.com>
2023-09-09 15:35:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,12 @@ streamlit.title('Merk\'s House of Horrors')
 streamlit.header('Breakfast Menu')
 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
 
